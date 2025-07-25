0079f079abda0002ce0172aa98ead713fa60af9b
Manish Devrani <manishdevrani777@gmail.com>
2025-07-25 22:43:22 +0530

    added database url in config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -6,4 +6,5 @@ export const config = {
   PORT: process.env.PORT ?? 8000,
   NODE_ENV: 'developement',
   ALLOWED_ORIGINS: ['http://localhost:3000'],
+  DATABASE_URL: process.env.DATABASE_URL ?? 'mongodb://localhost:27017',
 };
