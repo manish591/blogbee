d34324d17df9453fc52f7417fd49b1cb6f34be85
Manish Devrani <manishdevrani777@gmail.com>
2025-07-29 02:05:48 +0530

    added create user handler

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -5,8 +5,15 @@ dotenv.config();
 export const config = {
   PORT: process.env.PORT ?? 8000,
   LOG_LEVEL: process.env.LOG_LEVEL ?? 'info',
-  NODE_ENV: 'developement',
+  NODE_ENV: process.env.NODE_ENV ?? 'development',
   ALLOWED_ORIGINS: ['http://localhost:3000'],
   DATABASE_URL: process.env.DATABASE_URL ?? '',
   DATABASE_NAME: process.env.DB_NAME,
+  SESSION_COOKIE_OPTIONS: {
+    NAME: "sessionId",
+    MAX_AGE: 1000 * 60 * 60 * 24 * 30,
+    SAME_SITE: "lax" as const,
+    HTTP_ONLY: true,
+    SECURE: true
+  }
 };
