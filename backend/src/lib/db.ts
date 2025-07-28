1e0e06c5caa77c7f6a4fa5a47101c7ccdcd3c2c1
Manish Devrani <manishdevrani777@gmail.com>
2025-07-29 01:53:20 +0530

    fix formatting

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -1,19 +1,19 @@
 import * as mongoDB from 'mongodb';
 import { config } from '../config';
+import type { Blogs } from '../models/blogs.model';
+import type { Posts } from '../models/posts.model';
+import type { Session } from '../models/session.model';
+import type { Tags } from '../models/tags.model';
+import type { Users } from '../models/users.model';
 import { logger } from './winston';
-import { Users } from '../models/users.model';
-import { Session } from '../models/session.model';
-import { Blogs } from '../models/blogs.model';
-import { Posts } from '../models/posts.model';
-import { Tags } from '../models/tags.model';
 
 export const collections: {
-  users?: mongoDB.Collection<Users>
-  session?: mongoDB.Collection<Session>
-  blogs?: mongoDB.Collection<Blogs>
-  posts?: mongoDB.Collection<Posts>
-  tags?: mongoDB.Collection<Tags>
-} = {}
+  users?: mongoDB.Collection<Users>;
+  session?: mongoDB.Collection<Session>;
+  blogs?: mongoDB.Collection<Blogs>;
+  posts?: mongoDB.Collection<Posts>;
+  tags?: mongoDB.Collection<Tags>;
+} = {};
 
 export const client = new mongoDB.MongoClient(config.DATABASE_URL, {
   serverApi: {
@@ -29,15 +29,15 @@ export async function connectToDatabase() {
 
     const db: mongoDB.Db = client.db(config.DATABASE_NAME);
 
-    collections.users = db.collection<Users>("users");
-    collections.session = db.collection<Session>("session");
-    collections.blogs = db.collection<Blogs>("blogs");
-    collections.posts = db.collection<Posts>("posts");
-    collections.tags = db.collection<Tags>("tags");
+    collections.users = db.collection<Users>('users');
+    collections.session = db.collection<Session>('session');
+    collections.blogs = db.collection<Blogs>('blogs');
+    collections.posts = db.collection<Posts>('posts');
+    collections.tags = db.collection<Tags>('tags');
 
-    logger.info('Successfully connected to the database');
+    logger.info('Successfully Connected To The Database');
   } catch (err) {
-    logger.error('Failed to connect to the database', err);
+    logger.error('Failed To Connect To The Database', err);
     process.exit(1);
   }
 }
@@ -46,8 +46,8 @@ export async function disconnectFromDatabase() {
   try {
     await client.close();
 
-    logger.warn('Successfully disconnected from the database');
+    logger.warn('Successfully Disconnected From The Database');
   } catch (err) {
-    logger.error('Failed to disconnect from the database', err);
+    logger.error('Failed To Disconnect From The Database', err);
   }
 }
