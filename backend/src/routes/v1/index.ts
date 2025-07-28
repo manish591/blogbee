f16b4c9a65325f35303967bc83f2b975b11eee85
Manish Devrani <manishdevrani777@gmail.com>
2025-07-29 01:35:15 +0530

    removed unused routes

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import { blogsRoutes } from './blogs';
 import { userRoutes } from './users';
 
 const router = Router();
 
 router.use('/users', userRoutes);
-router.use('/blogs', blogsRoutes);
 
 export { router as v1Routes };
