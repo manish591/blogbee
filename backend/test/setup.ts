6c930b740ec9f722174e457c04c1a9d465bf687a
Manish Devrani <manishdevrani777@gmail.com>
2025-08-02 21:09:51 +0530

    added backend test setup file

---
+++ backend/test/setup.ts
@@ -0,0 +1,43 @@
+import {
+  MongoDBContainer,
+  type StartedMongoDBContainer,
+} from '@testcontainers/mongodb';
+import type * as mongo from 'mongodb';
+import { afterAll, beforeAll, beforeEach } from 'vitest';
+import { config } from '../src/config';
+import {
+  connectToDatabase,
+  createDatabaseClient,
+  disconnectFromDatabase,
+} from '../src/db';
+
+let db: mongo.Db;
+let dbClient: mongo.MongoClient;
+let mongodbContainer: StartedMongoDBContainer;
+
+const timeout = 60_000;
+
+beforeAll(async () => {
+  mongodbContainer = await new MongoDBContainer('mongo:8.0.12').start();
+  dbClient = createDatabaseClient(
+    `${mongodbContainer.getConnectionString()}/?directConnection=true`,
+    {
+      serverSelectionTimeoutMS: 10000,
+      connectTimeoutMS: 10000,
+    },
+  );
+  db = await connectToDatabase(dbClient, config.TEST_DATABASE_NAME);
+}, timeout);
+
+beforeEach(async () => {
+  // clean up db with fresh data
+  await db.dropDatabase();
+}, timeout);
+
+afterAll(async () => {
+  // close the db connections
+  await disconnectFromDatabase(dbClient);
+  await mongodbContainer?.stop();
+}, timeout);
+
+export { db };
