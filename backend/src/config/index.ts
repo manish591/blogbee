036b71defab9d0b384f9642115a2efd081142709
Manish Devrani <manishdevrani777@gmail.com>
2025-08-04 00:21:22 +0530

    updated config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -13,4 +13,5 @@ export const config = {
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  SESSION_EXPIRES_IN: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
 };
