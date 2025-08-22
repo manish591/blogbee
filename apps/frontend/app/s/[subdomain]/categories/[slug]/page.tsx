b85b7cbe0e15b938b70a4797ef861bdb6a9ee966
Manish Devrani <manishdevrani777@gmail.com>
2025-08-22 12:10:51 +0530

    added categories page

--- apps/frontend/app/s/[subdomain]/categories/[slug]/page.tsx
+++ apps/frontend/app/s/[subdomain]/categories/[slug]/page.tsx
@@ -1,7 +1,79 @@
-export default function CategoriesPage() {
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { PostCard } from '@/app/s/[subdomain]/_components/post-card';
+
+export default function PostCategoriesPage() {
   return (
-    <div>
-      <h1>categories page</h1>
+    <div className="py-24 max-w-7xl mx-auto px-8">
+      <div className="flex-1">
+        <div className="mb-12">
+          <h1 className="text-4xl font-bold leading-tight">Interviews</h1>
+        </div>
+      </div>
+      <div className="grid grid-cols-[minmax(0,1fr)_360px] gap-12">
+        <div>
+          <PostCard />
+          <span className="h-[1px] w-full border block my-8"></span>
+          <PostCard />
+          <span className="h-[1px] w-full border block my-8"></span>
+          <PostCard />
+
+          <div className="flex items-center justify-center mt-10">
+            <Button variant="outline" className="h-7 text-[0.8rem">
+              Load more
+              <ArrowRight />
+            </Button>
+          </div>
+        </div>
+        <aside className="w-64 space-y-8 py-2">
+          <div>
+            <h3 className="text-lg font-semibold mb-4">Categories</h3>
+            <ul className="space-y-2">
+              <li>
+                <Link
+                  href="#"
+                  className="text-sm text-foreground/70 hover:text-foreground"
+                >
+                  All
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-sm text-foreground/70 hover:text-foreground"
+                >
+                  Interviews
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-sm text-foreground/70 hover:text-foreground"
+                >
+                  Interviews
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-sm text-foreground/70 hover:text-foreground"
+                >
+                  Podcast
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-sm text-foreground/70 hover:text-foreground"
+                >
+                  Inspiration
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </aside>
+      </div>
     </div>
   );
 }
